09547b1aaf061ccedbafaef3a7ba62624ded6107
vravelo <vravelo@cgtscorp.com>
2019-09-05 17:02:20 -0400

    working server

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const net = require('net');
+const colors = require('colors')
 const port = 9000;
 
-let server = net.createServer((socket) => {
-  socket.write('saludo desde server');
+let server = net.createServer();
 
-})
+server.on('connection', (socket) => {
+  let remoteaddress = `${socket.remoteAddress}:${socket.remotePort}`
+  console.log(`new client is made ${remoteaddress}`.blue)
+
+  socket.on('data', (data) => {
+    console.log(`data from ${remoteaddress}  ${data}`.green)
+    socket.write(`hello ${remoteaddress} you send ${data}`)
+  })
+
+  socket.once('close', () => {
+    console.log(`connection closed from ${remoteaddress}`.white)
+  })
+  socket.on('error', (err) => {
+    console.error(`Connection ${remoteaddress} error ${err.message}`.red);
+
+  })
+});
+
+
+server.listen(port, () => {
+  console.log(`server listenig to ${server.address().port} port`.cyan)
+});
